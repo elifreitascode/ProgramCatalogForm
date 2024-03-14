c1b9f6c1fccf84be7c8e3989090a34a5b277cadd
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2024-03-13 22:08:48 -0300

    Inserção de dados no formulário  de cadastro de contatos

--- TDS-DS-Solucao/WinFormsBD/Form1.Designer.cs
+++ TDS-DS-Solucao/WinFormsBD/Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,23 +45,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
+            // dataGridView1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 81);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(776, 277);
-            this.listBox1.TabIndex = 1;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(713, 337);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Inserir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(748, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -66,7 +95,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- TDS-DS-Solucao/WinFormsBD/ScriptBancoDeDados.sql
+++ TDS-DS-Solucao/WinFormsBD/ScriptBancoDeDados.sql
@@ -1,2 +1,30 @@
-CREATE DATABASE db_tds_ds
+--create database db_tds_ds
+--go
+
+--use db_tds_ds
+--go
+
+--drop table tb_agenda
+--go
+
+/*
+create table tb_agenda (
+	codigo int constraint pk_agenda primary key identity(1,1),
+	nome varchar(100) not null, 
+	apelido varchar(100),
+	nascimento datetime,
+	telefones varchar(100) not null,
+	email varchar(100)
+)
+go
+
+insert into tb_agenda (nome, apelido, nascimento, telefones, email)
+values ('Samuel', 'Samuka', '1970-09-29', '9990-9900', 'samuka@example.com'),
+       ('Cristiano', 'Cris', '1969-09-18', '9991-9911', 'cristiano@example.com'),
+       ('Eduardo', 'Dudu', '2001-12-04', '9992-9922', 'dudu@example.com'),
+       ('Maria', 'Mariah', '1999-11-10', '9993-9933', 'mariah@example.com')
+go
+*/
+
+select * from tb_agenda
 go
